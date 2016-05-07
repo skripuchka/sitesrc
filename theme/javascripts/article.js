7c618ce65870064846b4e22da8e60ae287358de0
Oleg Pu <ol74eg@gmail.com>
2016-05-07 14:03:50 +0500

    Gallery preview

---
+++ theme/javascripts/article.js
@@ -0,0 +1,41 @@
+var imgs = [];
+var $currentImg;
+
+$(function () {
+    var images = $('#galleryContainer .item img').map(function(){
+        return $(this).attr('src');
+    });
+
+    imgs = images.toArray();
+});
+
+function showPreview(img) {
+    $currentImg = img;
+
+    $($('#overlayGallery #previewList .item img')[0]).attr('src', $currentImg);
+    $('#overlayGallery').show();
+}
+
+function nextImg() {
+    var idx = imgs.indexOf($currentImg);
+
+    if (idx == imgs.length - 1) {
+        closePreview();
+    } else {
+        showPreview(imgs[++idx]);
+    }
+}
+
+function prevImg(img) {
+    var idx = imgs.indexOf($currentImg);
+
+    if (idx == 0) {
+        closePreview();
+    } else {
+        showPreview(imgs[--idx]);
+    }
+}
+
+function closePreview() {
+    $('#overlayGallery').hide();
+}
